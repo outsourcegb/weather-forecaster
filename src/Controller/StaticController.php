60f49d4fee6af9394c6ae29a28ce37167256bfaf
Shashi <outsource.gb@gmail.com>
2024-08-09 14:55:49 +0530

    Static controller

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -8,18 +8,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class WeatherController extends AbstractController
+class StaticController extends AbstractController
 {
-    #[Route('/weather', name: 'app_weather')]
+    #[Route('/', name: 'app_static')]
     public function index(): Response
     {
         $draw = random_int(1, 100);
 
         $weather = $draw <= 50 ? "Its sunny!" : "Its rainy!";
 
-        return $this->render('weather/index.html.twig', [
-            'controller_name' => 'WeatherController',
-            'weather' => $weather,
+        return $this->render('static/index.html.twig', [
+            'controller_name' => 'StaticController',
         ]);
     }
 }
